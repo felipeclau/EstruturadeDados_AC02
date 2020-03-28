9034b704056652bab2731d181805a4934cb15dad
felipeclau <felipeclau@github.com>
2020-03-28 12:36:38 -0300

    inclusao_arquivos

---
+++ AC02_01.py
@@ -0,0 +1,19 @@
+# Disciplina: ESTRUTURA DE DADOS
+# Turma: BD3B NOITE
+# Nome Aluno: FELIPE CHIN LAU
+# Professor: JORGE CARLOS VALVERDE REBAZA
+# Data entrega: 31/03/2020
+
+'''
+PERGUNTA 1: Likes de Postagens em Redes Sociais
+'''
+
+def menu():
+    print('-='*17)
+    print('Menu Sistema Gestor de Postagens')
+    print('    1) Criar um post \n    2) Dar likes a um post \n    3) Top posts com mais likes')
+    x = input('\nDigite uma opção: ')
+    print('-='*17)
+    return x
+
+menu()
---
+++ AC02_02.py
@@ -0,0 +1,9 @@
+# Disciplina: ESTRUTURA DE DADOS
+# Turma: BD3B NOITE
+# Nome Aluno: FELIPE CHIN LAU
+# Professor: JORGE CARLOS VALVERDE REBAZA
+# Data entrega: 31/03/2020
+
+'''
+PERGUNTA 2: Rover Curiosity
+'''
