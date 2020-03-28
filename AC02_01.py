86505149a1ea4e5d644a15e55c1c56515941109a
felipeclau <felipeclau@github.com>
2020-03-28 17:47:33 -0300

    2308-1747

--- AC02_01.py
+++ AC02_01.py
@@ -10,21 +10,39 @@ PERGUNTA 1: Likes de Postagens em Redes Sociais
 
 
 class Post:
-    def __init__(self):
-        pass
+    def __init__(self, indice, like):
+        self.indice = indice
+        self.like = like
     
-# função para criar um menu.
-    def menu(self):
-        print('-='*17)
-        print('Menu Sistema Gestor de Postagens')
-        print('    1) Criar um post \n    2) Dar likes a um post \n    3) Top posts com mais likes')
-        opcao = input('\nDigite uma opção: ')
-        print('-='*17)
-        return opcao
 # função para criar um post
     def registroPost(self, opcao):
         post = []
         if opcao == 1:
             post.append(0)
-            print('Post número ', len(post), ' criado.')
-                        
+            print('Post número {} criado.'.format(len(post)))
+            print('A lista de postagens é a seguinte:')
+
+# função com o resultado após a execução de uma das opções do menu.
+    def resultado(self, indice, likes):
+            self.indice()
+            self.likes()
+            pass
+        
+# função para dar likes
+    def darLikes(self):
+        pass
+    
+# função top posts com mais likes
+    def topLikes(self):
+        pass
+            
+
+
+# função para criar um menu.
+def menu(self):
+    print('-='*17)
+    print('Menu Sistema Gestor de Postagens')
+    print('    1) Criar um post \n    2) Dar likes a um post \n    3) Top posts com mais likes')
+    opcao = input('\nDigite uma opção: ')
+    print('-='*17)
+    return opcao
--- main.py
+++ main.py
@@ -2,15 +2,49 @@ def menu():
     print('-='*17)
     print('Menu Sistema Gestor de Postagens')
     print('    1) Criar um post \n    2) Dar likes a um post \n    3) Top posts com mais likes')
-    opcao = input('\nDigite uma opção: ')
+    opcao = int(input('\nDigite uma opção: '))
     print('-='*17)
     return opcao
 # função para criar um post
-def registroPost(opcao):
-    post = []
-    if opcao == 1:
-        post.append(0)
-        return('Post número',len(post),' criado.')
+def registroPost(postagem):
+        postagem.append(0)
+        print('Post número {} criado.'.format(len(postagem)))
 
-x = menu()
-print(registroPost(x))
+# função com o resultado após a execução de uma das opções do menu.
+    def resultado(self, indice, likes):
+            self.indice()
+            self.likes()
+            pass
+        
+
+# função para dar likes
+    def darLikes(postagem, indicepostagem):
+        postagem[indicepostagem]
+    
+
+# função sub menu
+def submenu():
+    print('A lista de postagens é o seguinte: {}', postagem)
+    print('Índice: \nLikes: ')
+    opc = ''
+    while True:
+        opc = str(input('Aperta R para voltar ou S para sair: ')).upper().strip()
+        if opc !='R' or opc != 'S':
+            if opc == 'R':
+                menu()
+                break
+            if opc == 'S':
+                break
+            else:
+                print('Opção inválida!')
+            
+
+
+    
+
+        
+
+# função top posts com mais likes
+    def topLikes(self):
+        pass
+            
