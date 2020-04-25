0f95f8b75444154091752f9f9fe9875c00144da8
felipeclau <felipeclau@github.com>
2020-04-25 15:59:09 -0300

    AC03

---
+++ AC03.py
@@ -0,0 +1,10 @@
+# Disciplina: ESTRUTURA DE DADOS
+# Turma: BD3B NOITE
+# Nome Alunos: BRUNO BARROCA
+#              FELIPE CHIN LAU
+#              LETICIA MARQUES
+# Professor: JORGE CARLOS VALVERDE REBAZA
+# Data entrega: 28/04/2020
+
+''' Pergunta 1: Ranking de Algoritmos de Ordenação'''
+
--- teste.py
+++ teste.py
@@ -1,12 +1,24 @@
 class Lixo:
     # contrutor
     def ___init___(self):
-        self.pilhaLixoMetal = []               # Lista para lixo do tipo metal.
+        self.pilhaLixoMetal = []              # Lista para lixo do tipo metal.
         self.pilhaLixoFunk = []                # Lista para lixo do tipo não metal.
 
 # função para incluir lixo metal
-    def incluirLixoMetal(self, pesoLixo):
-        self.pilhaLixoMetal.append(pesoLixo)
+    def incluirLixoMetal(self, valor):
+        self.pilhaLixoFunk.append(valor)
+
+class Figuras:
+    #construtor
+    def __init__(self):
+        self.lista_quadrados = []
+
+    #metodos
+    def setQuadrado(self, quadrado):
+        self.lista_quadrados.append(quadrado)
 
 x = Lixo()
-x.incluirLixoMetal(2)
+#x.incluirLixoMetal(2)
+
+y = Figuras()
+y.lista_quadrados(2)
